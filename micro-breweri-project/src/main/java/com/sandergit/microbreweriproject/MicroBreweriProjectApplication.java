ea6aaa6e78e4a753749dd2df97edef494a4e715d
Sander Kaljula <sanderkaljula180@gmail.com>
2021-07-22 17:40:38 +0300

    first commit

---
+++ micro-breweri-project/src/main/java/com/sandergit/microbreweriproject/MicroBreweriProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sandergit.microbreweriproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBreweriProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBreweriProjectApplication.class, args);
+	}
+
+}
---
+++ micro-breweri-project/src/test/java/com/sandergit/microbreweriproject/MicroBreweriProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandergit.microbreweriproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBreweriProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
